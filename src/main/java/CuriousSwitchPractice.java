e9bdcd21c6bbfadd33f22e523b892d74b654adcf
shiddharrtha <67837601+shiddharrtha@users.noreply.github.com>
2022-09-15 09:36:06 -0400

    Initial commit

---
+++ src/main/java/CuriousSwitchPractice.java
@@ -0,0 +1,30 @@
+
+
+public class CuriousSwitchPractice {
+
+
+        //we have seen if-else and if-else if conditions
+        //these are great for making checks and proceeding differently
+        //depending upon the result of the check
+
+        //we can achieve similar result with another type of statement
+        //a switch statement checks a single value against multiple cases
+        //to find a match or not
+        //syntax is
+        //switch which is a keyword
+        //(expression) which hold the value we will check, expression can be any variable or literal
+        //case which is a keyword
+        //value_for_case: checks the expression against this
+        //statements to happen is the expression matches a case
+        //break; keyword to leave the switch and avoid unintended consequences
+        //default: keyword and case that happens if no match is found, like an else statement
+
+
+
+        //if we forget to use this keyword we will end up falling through the cases until we hit a break
+
+
+        //we can test multiple cases for the same result
+        //if any of the cases match we will implement that result
+
+}
